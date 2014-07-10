22c126a2d64bbc4f686463dbd005f79edbb8ff98
Jim V <vlandham@gmail.com>
2014-07-09 22:58:59 -0700

    first

---
+++ R/vggtheme.R
@@ -0,0 +1,17 @@
+
+
+vtheme <- theme_economist(horizontal=FALSE, base_size=10)
+#vtheme$plot.background$fill <- "#cccccc"
+#vtheme$axis.title$hjust <- 5
+vtheme$axis.text.y$hjust <- 1
+#vtheme$plot.title$hjust <- 0.06
+vtheme$axis.text.x$size <- 16
+vtheme$axis.title.x$size <- 16
+
+htheme <- theme_economist(base_size=10)
+#vtheme$plot.background$fill <- "#cccccc"
+#vtheme$axis.title$hjust <- 5
+htheme$axis.text.y$hjust <- 1
+#vtheme$plot.title$hjust <- 0.06
+htheme$axis.text.x$size <- 16
+htheme$axis.title.x$size <- 16
